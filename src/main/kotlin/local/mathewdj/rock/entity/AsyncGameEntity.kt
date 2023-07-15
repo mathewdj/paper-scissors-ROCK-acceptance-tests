db70524316498e27858ad0ae67781f1bc44803fb
mat <9652156+mathewdj@users.noreply.github.com>
2023-07-15 20:53:46 +1000

    Detekt fixes

--- src/main/kotlin/local/mathewdj/rock/entity/AsyncGameEntity.kt
+++ src/main/kotlin/local/mathewdj/rock/entity/AsyncGameEntity.kt
@@ -1,6 +1,12 @@
 package local.mathewdj.rock.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import local.mathewdj.rock.domain.Attack
 import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.type.SqlTypes
@@ -10,21 +16,21 @@ import java.util.*
 @Entity(name = "async_game")
 data class AsyncGameEntity(
         @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    var id: UUID? = null,
+        @GeneratedValue(strategy = GenerationType.UUID)
+        var id: UUID? = null,
 
-    @Column(columnDefinition = "VARCHAR(36)")
-    @JdbcTypeCode(SqlTypes.VARCHAR)
-    val gameId: UUID,
+        @Column(columnDefinition = "VARCHAR(36)")
+        @JdbcTypeCode(SqlTypes.VARCHAR)
+        val gameId: UUID,
 
-    @Column(columnDefinition = "VARCHAR(36)")
-    @JdbcTypeCode(SqlTypes.VARCHAR)
-    val playerId: UUID,
+        @Column(columnDefinition = "VARCHAR(36)")
+        @JdbcTypeCode(SqlTypes.VARCHAR)
+        val playerId: UUID,
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    val playerAttack: Attack,
+        @Column(nullable = false)
+        @Enumerated(EnumType.STRING)
+        val playerAttack: Attack,
 
-    @Column(nullable = false)
-    var createdAtUtc: LocalDateTime = LocalDateTime.now()
+        @Column(nullable = false)
+        var createdAtUtc: LocalDateTime = LocalDateTime.now()
 )
--- src/test/kotlin/local/mathewdj/rock/acceptance/AsyncGameStepDefinitions.kt
+++ src/test/kotlin/local/mathewdj/rock/acceptance/AsyncGameStepDefinitions.kt
@@ -8,16 +8,16 @@ import local.mathewdj.rock.domain.Attack
 class AsyncGameStepDefinitions {
     @Given("player {player}")
     fun `given player`(player: Player) {
-
+        TODO("$player")
     }
 
     @When("{player} plays {attack}")
-    fun `player uses attack`(attack: Attack) {
-
+    fun `player uses attack`(player: Player, attack: Attack) {
+        TODO("$player $attack")
     }
 
     @Then("{player} wins")
-    fun `{player} wins`() {
-
+    fun `{player} wins`(player: Player) {
+        TODO("$player")
     }
 }
