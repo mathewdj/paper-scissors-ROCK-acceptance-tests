219c705360fec7e91dd6eed92342ae3f6fef7262
mat <9652156+mathewdj@users.noreply.github.com>
2023-07-15 16:34:44 +1000

    Setup project

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+
+    kotlin("plugin.spring") version "1.8.21"
+    kotlin("plugin.jpa") version "1.8.21"
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+
+    id("org.flywaydb.flyway") version "9.19.0"
+}
+
+group = "local.mathewdj.paper.scissors.rock"
+version = "0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:7.2.0"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("org.flywaydb:flyway-core")
+
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+
+    testImplementation("io.cucumber:cucumber-java:7.12.0")
+    testImplementation("io.cucumber:cucumber-junit:7.12.0")
+
+    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
+    testImplementation("org.testcontainers:postgresql:1.17.6")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
+
+    testImplementation("io.mockk:mockk:1.13.5")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("SKIPPED", "FAILED")
+    }
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        txt.required.set(true)
+        md.required.set(true)
+    }
+}
