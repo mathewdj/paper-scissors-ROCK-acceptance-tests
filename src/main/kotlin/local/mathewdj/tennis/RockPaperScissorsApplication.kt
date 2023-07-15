1b837c3a9e6da10c07219335833aa72e459c1ca9
mat <9652156+mathewdj@users.noreply.github.com>
2023-07-15 19:11:30 +1000

    Create spring boot app

---
+++ src/main/kotlin/local/mathewdj/tennis/RockPaperScissorsApplication.kt
@@ -0,0 +1,12 @@
+package local.mathewdj.tennis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RockPaperScissorsApplication
+
+fun main() {
+    runApplication<RockPaperScissorsApplication>()
+}
+
